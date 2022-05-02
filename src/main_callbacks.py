41f5bcf00f0ebcf6872aa698b78ceb71706c2549
n <np00004@mix.wvu.edu>
2022-05-01 22:15:41 -0400

    add sale lookup

--- src/main_callbacks.py
+++ src/main_callbacks.py
@@ -3,7 +3,7 @@ import tkinter.ttk as ttk
 from PIL import ImageTk, Image
 import sqlite3 as sq
 
-def get_sale_info(txt_out):
+def get_emp_info(txt_out):
 	# Why does this want a float!?!?
 	con = sq.connect('../test_db/storeDB.db')
 	cur = con.cursor()
@@ -14,4 +14,21 @@ def get_sale_info(txt_out):
 		txt_out.insert(tk.END, str(each) + "\n")
 
 	con.close()
+def get_sale_info(txt_out, num):
+	# Run SQL query to get all sales with a dollar amount greater than num
+	con = sq.connect('../test_db/storeDB.db')
+	cur = con.cursor()
+	num = int(num)
+	results = cur.execute('''
+						SELECT Sales.date, Sales.dollar_amount,
+						Sales.last_four_card
+						FROM sales
+						WHERE sales.dollar_amount > {:d} 
+					   '''.format(num))
+	txt_out.delete(0.0, tk.END)
+
+	for each in results:
+		txt_out.insert(tk.END, str(each) + "\n")
+	con.close()
+
 	return
--- src/main_layout.py
+++ src/main_layout.py
@@ -13,18 +13,31 @@ def main_setup(in_frame):
     txt_out = tk.Text(in_frame,bg="black", fg="white")
     txt_out.grid(row=3,column=0,columnspan=3)
 
-    sale_info_frame = tk.Frame(in_frame,bg="orange",width=W,height=H)
-    sale_info_frame.grid(row=1,column=0)
+    emp_info_frame = tk.Frame(in_frame,bg="orange",width=W,height=H)
+    emp_info_frame.grid(row=1,column=0)
 
-    sale_info_lbl = tk.Label(sale_info_frame, text="Get general sales info")
-    sale_info_lbl.grid(row=0,column=0)
+    emp_info_lbl = tk.Label(emp_info_frame, text="Get list of employees!")
+    emp_info_lbl.grid(row=0,column=0)
 
-    select_btn = tk.Button(sale_info_frame, text="Go!",
-                           command=lambda:get_sale_info(txt_out))
+    select_btn = tk.Button(emp_info_frame, text="Go!",
+                           command=lambda:get_emp_info(txt_out))
     select_btn.grid(row=1,column=0)
 
-    salary_info = tk.Frame(in_frame,bg="purple", width=W, height=H)
-    salary_info.grid(row=1,column=1)
+
+    sale_info_frame = tk.Frame(in_frame,bg="purple", width=W, height=H)
+    sale_info_frame.grid(row=1,column=1)
+
+    sale_info_lbl = tk.Label(sale_info_frame, text="Find All Sales Greater than")
+    sale_info_lbl.grid(row=0, column=0, columnspan=2)
+
+    sale_info_in = tk.Entry(sale_info_frame)
+    sale_info_in.grid(row=1,column=1)
+
+    sale_info_btn = tk.Button(sale_info_frame, bg="purple", fg="orange",
+                                text="Go!", 
+                              command=lambda:get_sale_info(txt_out,
+                                                           sale_info_in.get()))
+    sale_info_btn.grid(row=1,column=0)
 
     dept_info = tk.Frame(in_frame,bg="green", width=W, height=H)
     dept_info.grid(row=1,column=2)
@@ -39,8 +52,8 @@ def main_setup(in_frame):
     frame6.grid(row=2,column=2)
 
 
+    #new_lbl = tk.Label(emp_info_frame, text="hi")
     new_lbl = tk.Label(sale_info_frame, text="hi")
-    new_lbl = tk.Label(salary_info, text="hi")
 
 
     #output = tk.Text(in_frame,width=100,height=50)
